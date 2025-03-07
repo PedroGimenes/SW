668d781b1c987371fd76cc0f95c6cde911e79780
PedroGimenes <pedro.gimenes@etec.sp.gov.br>
2025-03-07 16:53:28 -0300

    exercicios

---
+++ aula04/exe01.php
@@ -0,0 +1,11 @@
+<?php
+$nome = $_GET ['nam'];
+$num1= $_GET ['num1'];
+$num2= $_GET ['num2'];
+$num3= $_GET ['num3'];
+
+
+    echo"Bem vindo, $nome , sua média é :  ";
+
+
+?>
---
+++ aula04/exe03.php
@@ -0,0 +1,7 @@
+<?php
+$nome = $_POST ['nome'];
+$senha = $_POST['senha'];
+
+
+
+?>
