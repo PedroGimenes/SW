bd329ee151095e95a45471abbdd62cc54569943b
PedroGimenes <pedro.gimenes@etec.sp.gov.br>
2025-05-09 18:28:56 -0300

    terminei

---
+++ Aula09/Lampada.class.php
@@ -0,0 +1,33 @@
+<?php
+    class Lampada{
+
+        public $Fabricante;
+        public $Tensao;
+        public $Potencia;
+        public $Cor;
+        public $Status;
+
+        public function Ligar(){
+            $this->Status = True;
+        }
+        public function Desligar(){
+            $this->Status = False;
+        }
+        public function MostrarDados(){
+             echo "FABRICANTE: " . $this->Fabricante . "<br>";
+             echo "TENSÃo: " . $this->Tensao . "<br>";
+             echo "POTÊNCIA: " . $this->Potencia . "<br>";
+             echo "COR: " . $this->Cor . "<br>";
+             if ($this->Status == 1){
+                echo "STATUS DA LAMPADA: LIGADA <br>";
+             }else{
+                echo "STATUS DA LAMPADA: DESLIGADA <br>";
+             }
+
+        }
+        
+
+    }
+
+
+?>
